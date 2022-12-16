2bc932bd8046eb356588ac21670e29b220ea82c7
aymane-daif <aymaned345@gmail.com>
2022-12-16 18:15:59 +0100

    :ok: add styles; customize bootstrap default

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { useKeycloak } from '@react-keycloak/web';
+import ListCourse from './components/ListCourse';
 import Loader from './shared/loader/loader';
 
 function App({ children }) {
-  const { initialized } = useKeycloak();
+  // const { initialized } = useKeycloak();
 
-  if (!initialized) {
-    return <Loader />;
-  }
-  return { children };
+  // if (!initialized) {
+  //   return <Loader />;
+  // }
+  // return { children };
+  return <ListCourse />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { ReactKeycloakProvider } from '@react-keycloak/web';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import ControlledRoute from './security/controlledRoute';
 import keycloak from './security/keycloak-config';
 import { routes } from './route/routes';
@@ -13,28 +12,32 @@ import NotFound from './shared/not-found/notFound';
 import store from './store';
 import reportWebVitals from './reportWebVitals';
 
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/sass/style.scss';
+import './assets/sass/style.react.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ReactKeycloakProvider authClient={keycloak}>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App>
-          {getEffectiveRoutes(routes).map(
-            ({ path, Component, exact, roles }) => (
-              <ControlledRoute
-                key={path}
-                exact={exact}
-                path={`${process.env.PUBLIC_URL}${path}`}
-                roles={roles}>
-                <Component />
-              </ControlledRoute>
-            )
-          )}
-          <Route component={NotFound} />
-        </App>
-      </BrowserRouter>
-    </Provider>
-  </ReactKeycloakProvider>
+  // <ReactKeycloakProvider authClient={keycloak}>
+  //   <Provider store={store}>
+  //     <BrowserRouter>
+  //       <App>
+  //         {getEffectiveRoutes(routes).map(
+  //           ({ path, Component, exact, roles }) => (
+  //             <ControlledRoute
+  //               key={path}
+  //               exact={exact}
+  //               path={`${process.env.PUBLIC_URL}${path}`}
+  //               roles={roles}>
+  //               <Component />
+  //             </ControlledRoute>
+  //           )
+  //         )}
+  //         <Route component={NotFound} />
+  //       </App>
+  //     </BrowserRouter>
+  //   </Provider>
+  // </ReactKeycloakProvider>
+  <App />
 );
 
 // If you want to start measuring performance in your app, pass a function

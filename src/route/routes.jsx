aba7ea74634fe1ebe2981573ec2e69768c379575
Zakaria Dani <zakaria.dani2015@gmail.com>
2022-12-22 20:16:43 +0100

    add profile/settings page :boom:

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -1,11 +1,19 @@
-import { createBrowserRouter } from 'react-router-dom';
-import ListCourses from '../components/ListCourses';
-import Home from '../pages/Home';
-import Registration from '../pages/Registration';
+import { createBrowserRouter } from "react-router-dom";
+import ListCourses from "../components/ListCourses";
+import Home from "../pages/Home";
+import Registration from "../pages/auth/Registration";
+import ProfilePage from "../pages/profile/ProfilePage";
+import { ProfileHeader } from "../components/profile/ProfileHeader";
 
-export const routes = createBrowserRouter(
-  [
-    { path: '/', element:<ListCourses />, exact: false },
-    { path: '/auth/register', element: <Registration />, exact: false },
-  ]
-);
+export const routes = createBrowserRouter([
+  { path: "/", element: <ListCourses />, exact: false },
+  { path: "/auth/register", element: <Registration />, exact: false },
+  {
+    path: "/profile/*",
+    children: [
+      { path: "settings", element: <ProfileHeader />, exact: false },
+    ],
+    element: <ProfilePage />,
+    exact: false,
+  },
+]);

940a4d47a7399da0d9d232381cf6f2e000fb1d39
aymane-daif <aymaned345@gmail.com>
2022-12-23 23:30:45 +0100

    :ok: change not found component

--- src/route/routes.jsx
+++ src/route/routes.jsx
@@ -2,6 +2,6 @@ import ListCourses from '../components/ListCourses';
 import Registration from '../pages/Registration';
 
 export const routes = [
-  { path: '/', Component: ListCourses, exact: false },
-  { path: '/auth/register', Component: Registration, exact: false },
+  { path: '/', Component: ListCourses, exact: true },
+  { path: '/auth/register', Component: Registration, exact: true },
 ];
--- src/shared/not-found/notFound.jsx
+++ src/shared/not-found/notFound.jsx
@@ -1,33 +1,72 @@
 import React from 'react';
 import { Fragment } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { Button, Col, Container } from 'react-bootstrap';
-
+import { toAbsoluteUrl } from '../../../_metronic/helpers';
 const NotFound = () => {
   return (
-    <Fragment>
-      <div className='page-wrapper'>
-        <div className='error-wrapper'>
-          <Container>
-            <div className='error-heading'>
-              <h2 className='headline font-danger'>{'404'}</h2>
+    <div className='d-flex flex-column flex-root'>
+      <div
+        className='d-flex flex-column flex-column-fluid bgi-position-y-bottom position-x-center bgi-no-repeat bgi-size-contain bgi-attachment-fixed'
+        style={{
+          backgroundImage: `url('${toAbsoluteUrl(
+            '/media/illustrations/progress-hd.png'
+          )}')`,
+        }}>
+        <div className='d-flex flex-column flex-column-fluid text-center p-10 py-lg-20'>
+          <a href='/dashboard' className='mb-10 pt-lg-20'>
+            <img
+              alt='Logo'
+              src={toAbsoluteUrl('/media/logos/logo-1.svg')}
+              className='h-50px mb-5'
+            />
+          </a>
+          <div className='pt-lg-10 mb-10'>
+            <Outlet />
+            <div className='text-center'>
+              <Link to='/' className='btn btn-lg btn-primary fw-bolder'>
+                Go to homepage
+              </Link>
             </div>
-            <Col md='8 offset-md-2'>
-              <p className='sub-content'>
-                {
-                  'The page you are attempting to reach is currently not available. This may be because the page does not exist or has been moved.'
-                }
-              </p>
-            </Col>
-            <Link to={`${process.env.PUBLIC_URL}/`}>
-              <Button color='danger-gradien' size='lg'>
-                Return to the main page
-              </Button>
-            </Link>
-          </Container>
+          </div>
+          <div
+            className='
+          d-flex
+          flex-row-auto
+          bgi-no-repeat
+          bgi-position-x-center
+          bgi-size-contain
+          bgi-position-y-bottom
+          min-h-100px min-h-lg-350px
+        '
+            style={{
+              backgroundImage: `url('${toAbsoluteUrl(
+                '/media/illustrations/sketchy-1/17.png'
+              )}')`,
+            }}></div>
+        </div>
+
+        <div className='d-flex flex-center flex-column-auto p-10'>
+          <div className='d-flex align-items-center fw-bold fs-6'>
+            <a
+              href='https://keenthemes.com'
+              className='text-muted text-hover-primary px-2'>
+              About
+            </a>
+            <a
+              href='mailto:support@keenthemes.com'
+              className='text-muted text-hover-primary px-2'>
+              Contact
+            </a>
+            <a
+              href='https://1.envato.market/EA4JP'
+              className='text-muted text-hover-primary px-2'>
+              Contact Us
+            </a>
+          </div>
         </div>
       </div>
-    </Fragment>
+    </div>
   );
 };
 
